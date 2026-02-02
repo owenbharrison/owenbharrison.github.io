c86c21ceb50ec2a24e4656b1ee4d7633d2c87f3e
Owen Harrison <owenbharrison@gmail.com>
2026-02-01 18:19:30 -0700

    sokol_testing: new gizmo/cubemap version

--- sims_and_games/sokol_testing/sokol.js
+++ sims_and_games/sokol_testing/sokol.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\owenb\AppData\Local\Temp\tmpgfnmzcc7.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmpr6zryfc_.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -216,21 +216,21 @@ Module['FS_createPath']("/assets", "models", true, true);
 
   })();
 
-// end include: C:\Users\owenb\AppData\Local\Temp\tmpgfnmzcc7.js
-// include: C:\Users\owenb\AppData\Local\Temp\tmpctazgj5p.js
+// end include: C:\Users\owenb\AppData\Local\Temp\tmpr6zryfc_.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmpo6faigti.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\owenb\AppData\Local\Temp\tmpctazgj5p.js
-// include: C:\Users\owenb\AppData\Local\Temp\tmpo_pdr9wa.js
+  // end include: C:\Users\owenb\AppData\Local\Temp\tmpo6faigti.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmpcszfdcvv.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\owenb\AppData\Local\Temp\tmpo_pdr9wa.js
+  // end include: C:\Users\owenb\AppData\Local\Temp\tmpcszfdcvv.js
 
 
 var arguments_ = [];
@@ -7598,8 +7598,6 @@ var wasmImports = {
   sapp_js_clear_favicon,
   /** @export */
   sapp_js_destroy_custom_mouse_cursor,
-  /** @export */
-  sapp_js_exit_pointerlock,
   /** @export */
   sapp_js_init,
   /** @export */
