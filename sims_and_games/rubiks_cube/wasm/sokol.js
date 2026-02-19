20883adf0af467f70ed7929b65d4e8f4b1bff33c
Owen Harrison <owenbharrison@gmail.com>
2026-02-18 17:52:49 -0700

    rubiks_cube: selectable sizes

--- sims_and_games/rubiks_cube/wasm/sokol.js
+++ sims_and_games/rubiks_cube/wasm/sokol.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\owenb\AppData\Local\Temp\tmpmuotvu_o.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmp6h_l0uxu.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -213,21 +213,21 @@ Module['FS_createPath']("/assets", "skybox", true, true);
 
   })();
 
-// end include: C:\Users\owenb\AppData\Local\Temp\tmpmuotvu_o.js
-// include: C:\Users\owenb\AppData\Local\Temp\tmpyeo4sqtb.js
+// end include: C:\Users\owenb\AppData\Local\Temp\tmp6h_l0uxu.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmp5x3u0dg5.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\owenb\AppData\Local\Temp\tmpyeo4sqtb.js
-// include: C:\Users\owenb\AppData\Local\Temp\tmpnh2i597v.js
+  // end include: C:\Users\owenb\AppData\Local\Temp\tmp5x3u0dg5.js
+// include: C:\Users\owenb\AppData\Local\Temp\tmpv1fnja1l.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\owenb\AppData\Local\Temp\tmpnh2i597v.js
+  // end include: C:\Users\owenb\AppData\Local\Temp\tmpv1fnja1l.js
 
 
 var arguments_ = [];
